6461f9c8a9c96036300a7075d98c51f667f2ff0e
Shiv Pratap <sp09singhwaghel@gmail.com>
2024-04-17 23:28:05 +0530

    add: basic setup of application

---
+++ index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import { MONGO_DB_URL } from "./config";
+import mongoose from "mongoose";
+import { AdminRoute, VendorRoute } from "./routes";
+import { DbConnect } from "./config/dbConnect";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/admin", AdminRoute);
+app.use("/vendor", VendorRoute);
+
+app.listen(8000, async () => {
+  console.log("App is running on port 8000");
+  await DbConnect(MONGO_DB_URL);
+});
