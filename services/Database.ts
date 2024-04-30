2a87692b503273f9f23c4b19386265a59d6f5ff4
Shiv Pratap <sp09singhwaghel@gmail.com>
2024-04-30 22:26:46 +0530

    add extra error handling functions for mongoose

--- services/Database.ts
+++ services/Database.ts
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 export async function DbConnect(MONGO_DB_URL: string) {
   try {
+    mongoose.connection.on("connected", () => console.log("connected"));
+    mongoose.connection.on("disconnected", (err) => console.log("disconnected",err));
+
     await mongoose.connect(MONGO_DB_URL);
     console.log("Db connected successfully");
   } catch (error) {
